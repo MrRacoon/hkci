560a1030454b5255ca48ddb3447b2c35fd14094f
Erik Sutherland <erik.misu@gmail.com>
2017-01-04 00:02:00 -0800

    add shallow test suite.. (doesn't run any tests)

--- test/haskind.js
+++ test/haskind.js
@@ -1,11 +1,41 @@
-const mocha = require('mocha');
+const should = require('should');
+const repl = require('../lib/hkci');
+should();
 
-const describe = mocha.describe;
-const it = mocha.it;
-
-describe('', function () {
-    it('', function () {
-
+xdescribe('hkci', () => {
+  describe('context', () => {
+    it('includes Data', () => {
+      repl.context.should.exist;
     });
+    it('includes Bool', () => {
+      repl.context.Bool.should.exist;
+    });
+    it('includes Either', () => {
+      repl.context.Either.should.exist;
+    });
+    it('includes Eq', () => {
+      repl.context.Eq.should.exist;
+    });
+    it('includes Ix', () => {
+      repl.context.Ix.should.exist;
+    });
+    it('includes List', () => {
+      repl.context.List.should.exist;
+    });
+    it('includes Map', () => {
+      repl.context.Map.should.exist;
+    });
+    it('includes Maybe', () => {
+      repl.context.Maybe.should.exist;
+    });
+    it('includes Ord', () => {
+      repl.context.Ord.should.exist;
+    });
+    it('includes String', () => {
+      repl.context.String.should.exist;
+    });
+    it('includes Tuple', () => {
+      repl.context.Tuple.should.exist;
+    });
+  });
 });
-
