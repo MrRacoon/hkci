35e7d3f45e5f61b1b3aaec2b2e08540b0b77107a
Erik Sutherland <erik.misu@gmail.com>
2017-01-08 22:17:07 -0800

    refine available options/modules

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 
 const opt = require('node-getopt').create([
-    ['c' , 'cwd'    , 'run hkci in the context of a haskind project'],
-    ['h' , 'help'   , 'display this help'],
+    ['c' , 'cwd' , 'run hkci in the context of a haskind project'],
+    ['h' , 'help' , 'display this help'],
     ['v' , 'version', 'show version'],
 
-    ['D' , 'Data'   , 'preload Data'],
+    ['D' , 'Data' , 'preload Data'],
+    ['C' , 'Control' , 'preload Data'],
+    ['S' , 'System' , 'preload Data'],
+    ['P' , 'Prelude' , 'preload Prelude'],
 
-    ['b' , 'Bool'   , 'preload Data.Bool'],
+    ['b' , 'Bool' , 'preload Data.Bool'],
     ['E' , 'Either' , 'preload Data.Either'],
-    ['e' , 'Eq'     , 'preload Data.Eq'],
-    ['x' , 'Ix'     , 'preload Data.Ix'],
-    ['l' , 'List'   , 'preload Data.List'],
-    ['m' , 'Map'    , 'preload Data.Map'],
-    ['M' , 'Maybe'  , 'preload Data.Maybe'],
+    ['e' , 'Enum' , 'preload Data.Enum'],
+    ['e' , 'Eq' , 'preload Data.Eq'],
+    ['f' , 'Function', 'preload Data.Function'],
+    ['x' , 'Ix' , 'preload Data.Ix'],
+    ['l' , 'List' , 'preload Data.List'],
+    ['m' , 'Map' , 'preload Data.Map'],
+    ['M' , 'Maybe' , 'preload Data.Maybe'],
+    ['o' , 'Ord' , 'preload Data.Ord'],
     ['s' , 'String' , 'preload Data.String'],
-    ['o' , 'Ord'    , 'preload Data.Ord'],
-    ['T' , 'Tuple'  , 'preload Data.Tuple']
+    ['T' , 'Tuple' , 'preload Data.Tuple']
 ]).bindHelp().parseSystem();
 
 module.exports = require('./lib/hkci.js')(opt);
