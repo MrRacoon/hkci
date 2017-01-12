9abae0f60483ee3b6c12d6ebd30cbaf4a159fa24
Erik Sutherland <erik.misu@gmail.com>
2017-01-11 22:09:19 -0800

    add strict

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const opt = require('node-getopt').create([
     ['c' , 'cwd' , 'run hkci in the context of a haskind project'],
     ['h' , 'help' , 'display this help'],
@@ -21,25 +23,4 @@ const opt = require('node-getopt').create([
     ['T' , 'Tuple' , 'preload Data.Tuple']
 ]).bindHelp().parseSystem();
 
-if (opt.options.version) {
-  packageJson = require('../package.json');
-  console.log(packageJson.name, packageJson.version) // eslint-disable-line
-  return;
-}
-
-let packageJson;
-let haskind;
-if (opt.options.cwd) {
-  console.info('loading local haskind:', process.cwd()); // eslint-disable-line
-  packageJson = require(process.cwd() + '/package.json');
-  if (packageJson.name === 'haskind') {
-    haskind = require(process.cwd() + '/index.js');
-  }
-}
-
-if (!haskind) {
-  packageJson = require('haskind/package.json');
-  haskind     = require('haskind');
-}
-
-module.exports = require('./dist/hkci.js')(packageJson, haskind, opt);
+module.exports = require('./dist/hkci.js')(opt);
